f64469b34e9e20218c4c0f75fe2b1bc5087fec06
Tuan Shridhar <47683349+tshridhar@users.noreply.github.com>
2020-08-25 15:11:55 -0700

    Stable build, no add or drop functionality. Converted to txt input based model

--- autoreg.py
+++ autoreg.py
@@ -1,50 +1,65 @@
-SITE_URL = "https://www.reg.uci.edu/cgi-bin/webreg-redirect.sh"
-f = open("info.txt", 'r')
+""" FUTURE VERSION WILL BE HEADLESS """
+""" THIS IS STILL A TESTING BUILD   """
 
-USERNAME = f.readline().strip()
-PASSWORD = f.readline().strip()
-CLASSES = ["placeholder"]
+from pathlib import Path
+class Instruction:
+    def __init__(self, file, driver):
+        self._info = {"username" : [], "password" : [], "add" : [], "drop" : []}
+        self.curr_instr = ""
+        self.file = file
+        self.driver = driver
+        self._read_from_file()
 
-# pip install chromedriver-py==84.0.4147.30
+    def _read_from_file(self):
+        for i in self.file.readlines():
+            if self.curr_instr in self._info.keys() and i.strip() not in self._info.keys():
+                self._info[self.curr_instr].append(i.strip())
+            self.curr_instr = i.strip() if i.strip() in self._info.keys() else self.curr_instr
+
+    def _login(self):
+        user_form = self.driver.find_element_by_id("ucinetid")
+        pass_form = self.driver.find_element_by_id("password")
+        submit_button = self.driver.find_element_by_xpath("//input[@type ='submit']")
+        user_form.send_keys(self._info["username"])
+        pass_form.send_keys(self._info["password"])
+        submit_button.click()
+
+    def _logout(self):
+        logout_button = self.driver.find_element_by_xpath("//input[@value='Logout']")
+        logout_button.click()
+
+    def exec_instr(self):
+        self._login()
+        enrollment_menu_button = self.driver.find_element_by_xpath("//input[@value='Enrollment Menu']")
+        enrollment_menu_button.click()
+        self._logout()
+
+    ### "ADD" Functionality disabled temporarily ####
+
+    # add_class_radio = driver.find_element_by_id("add")
+    # add_class_radio.click()
+
+    # course_code_form = driver.find_element_by_xpath("//input[@name='courseCode']")
+    # course_code_form.send_keys(CLASSES[0])
+
+    # request_button = driver.find_element_by_xpath("//input[@value='Send Request']")
+    # request_button.click()
+
 from selenium import webdriver
 from chromedriver_py import binary_path
 
+# SETUP CONST VAR
+SITE_URL = "https://www.reg.uci.edu/cgi-bin/webreg-redirect.sh"
+FILENAME = "inp.txt"
+
+# INITIALIZE DRIVER
 driver = webdriver.Chrome(executable_path=binary_path)
 driver.get(SITE_URL)
-html = driver.page_source
-user_form = driver.find_element_by_id("ucinetid")
-pass_form = driver.find_element_by_id("password")
-submit_button = driver.find_element_by_xpath("//input[@type ='submit']")
 
-user_form.send_keys(USERNAME)
-pass_form.send_keys(PASSWORD)
-submit_button.click()
+# OPEN 
+f = open(Path(FILENAME), 'r')
 
-enrollment_menu_button = driver.find_element_by_xpath("//input[@value='Enrollment Menu']")
-enrollment_menu_button.click()
-
-### "ADD" Functionality disabled temporarily ####
-
-# add_class_radio = driver.find_element_by_id("add")
-# add_class_radio.click()
-
-# course_code_form = driver.find_element_by_xpath("//input[@name='courseCode']")
-# course_code_form.send_keys(CLASSES[0])
-
-# request_button = driver.find_element_by_xpath("//input[@value='Send Request']")
-# request_button.click()
-
-logout_button = driver.find_element_by_xpath("//input[@value='Logout']")
-logout_button.click()
+c = Instruction(f, driver)
+c.exec_instr()
 
 f.close()
-
-"""
-HTML FOR REFERENCE
-
-<input class="WebRegButton" value="Enrollment Menu" type=   "submit" name="submit">
-<input type="radio" name="mode" value="add" id="add">
-<input type="text" name="courseCode" size="5" maxlength="5">
-<input type="submit" name="button" value="Send Request">
-<input class="WebRegButton  WebRegLogoutButton" value="Logout" type="submit" name="submit">
-"""
